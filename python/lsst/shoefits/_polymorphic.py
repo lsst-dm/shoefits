9f0424a5f3bf40d8c35145bb063491982fdc57cd
Jim Bosch <jbosch@astro.princeton.edu>
2024-07-08 15:15:47 -0400

    Drop adapter-registry envvar/config mechanisms.
    This is better left to downstream code.

--- python/lsst/shoefits/_polymorphic.py
+++ python/lsst/shoefits/_polymorphic.py
@@ -22,7 +22,6 @@ __all__ = (
     "get_tag_from_registry",
 )
 
-import os
 import warnings
 from abc import abstractmethod
 from collections.abc import Callable
@@ -31,10 +30,8 @@ from typing import (
     Generic,
     Literal,
     Protocol,
-    TypeAlias,
     TypeVar,
     Union,
-    final,
     get_args,
     get_origin,
     overload,
@@ -44,9 +41,6 @@ import astropy.io.fits
 import pydantic
 import pydantic_core.core_schema as pcs
 
-from lsst.resources import ResourcePath
-from lsst.utils.doImport import doImportType
-
 from ._read_context import ReadContext, ReadError
 from ._write_context import WriteContext, WriteError
 from .json_utils import JsonValue
@@ -54,9 +48,6 @@ from .json_utils import JsonValue
 _C = TypeVar("_C", bound=type[Any])
 _T = TypeVar("_T")
 _S = TypeVar("_S", bound=pydantic.BaseModel)
-
-
-CONFIGS_ENVVAR: str = "SHOEFITS_ADAPTER_CONFIGS"
 
 _tag_registry: dict[type[Any], str] = {}
 
@@ -125,44 +116,6 @@ def register_tag(tag: str, target_type: _C | None = None) -> Any:
     else:
         _tag_registry[target_type] = tag
         return target_type
-
-
-@final
-class AdapterFactory(pydantic.BaseModel):
-    """A serializable factory for `PolymorphicAdapter` objects.
-
-    The adapter factory model represents the values of an adapter registry
-    file, which is a dictionary-like JSON file with tag strings as keys.  See
-    `PolymorphicAdapterRegistry` for details.  For adapters without any
-    constructor arguments or native polymorphic types (which are already
-    Pydantic models), simple string values are used.
-    """
-
-    type_: str = pydantic.Field(alias="type")
-    """Fully-qualified class name of the adapter type.
-    """
-
-    args: list[int | str | float | bool | None] = pydantic.Field(default_factory=list)
-    """Positional arguments to the adapter constructor."""
-
-    kwargs: dict[str, int | str | float | bool | None] = pydantic.Field(default_factory=dict)
-    """Keyword arguments to the adapter constructor."""
-
-    def make_adapter(self) -> PolymorphicAdapter[Any, Any]:
-        """Construct an adapter instance from this configuration."""
-        type_ = doImportType(self.type_)
-        if issubclass(type_, PolymorphicAdapter):
-            return type_(*self.args, **self.kwargs)
-        elif issubclass(type_, pydantic.BaseModel):
-            return NativeAdapter(type_)
-        else:
-            raise TypeError(
-                f"Cannot construct a PolymorphicAdapter from type {self.type_} that is neither an adapter "
-                "subclass or a Pydantic model."
-            )
-
-
-AdapterConfig: TypeAlias = str | AdapterFactory
 
 
 class PolymorphicAdapter(Generic[_T, _S]):
@@ -252,21 +205,6 @@ class PolymorphicAdapterRegistry:
 
     Notes
     -----
-    While the `register_adapter` and `register_native` methods may be used to
-    populate the registry directly, when loading an object may require
-    importing a module, it is recommended that adapter definitions are instead
-    declared in configuration files and the paths of those files added to the
-    ``SHOEFITS_ADAPTER_CONFIGS`` environment variable.  Config files are JSON
-    dictionaries, with string tags as keys and nested dictionaries
-    corresponding to the `AdapterFactory` model as values.
-
-    `PolymorphicAdapterRegistry` is not a singleton, though applications may
-    wish to construct a single registry at import time.  The set of all config
-    files is read from the ``SHOEFITS_ADAPTER_CONFIGS`` environment variable
-    when a registry is constructed (later updates to the environment variable
-    are ignored), and individual config files are read as needed (in the order
-    specified in the environment variable) and their contents cached.
-
     This registry provides a way for the [de]serialization machinery to go from
     a string tag to `PolymorphicAdapter` instance.  When saving objects, it
     also needs a way to obtain that string tag from the object being saved,
@@ -275,44 +213,10 @@ class PolymorphicAdapterRegistry:
     """
 
     def __init__(self) -> None:
-        self._validator = pydantic.TypeAdapter(dict[str, AdapterConfig])
-        config_path = os.environ.get("SHOEFITS_ADAPTER_CONFIGS", "")
-        if config_path:
-            self._config_files_unread = [ResourcePath(path) for path in config_path.split(":")]
-        else:
-            self._config_files_unread = []
-        self._config_files_read: list[str] = []
-        # PATH-like envvars are expected to be processed such that items in
-        # the beginning take priority over those in the back.  We reverse the
-        # list so we can use list.pop() to go from high to low priority.
-        self._config_files_unread.reverse()
-        self._adapter_factories: dict[str, AdapterFactory] = {}
         self._adapters: dict[str, PolymorphicAdapter[Any, pydantic.BaseModel]] = {}
 
     def __getitem__(self, tag: str) -> PolymorphicAdapter[Any, pydantic.BaseModel]:
-        adapter: PolymorphicAdapter[Any, pydantic.BaseModel] | None = None
-        if adapter := self._adapters.get(tag):
-            return adapter
-        if adapter_factory := self._adapter_factories.get(tag):
-            adapter = adapter_factory.make_adapter()
-            self._adapters[tag] = adapter
-            return adapter
-        while self._config_files_unread:
-            config_path = self._config_files_unread.pop()
-            if not config_path.exists():
-                continue
-            self._config_files_read.append(str(config_path))
-            for key, factory in self._validator.validate_json(config_path.read()).items():
-                if isinstance(factory, str):
-                    factory = AdapterFactory.model_construct(adapter_type=factory)
-                self._adapter_factories.setdefault(key, factory)
-                if key == tag:
-                    adapter = factory.make_adapter()
-            if adapter is not None:
-                return adapter
-        raise KeyError(
-            f"No adapter found for polymorphic field with tag {tag} in any of {self._config_files_read}."
-        )
+        return self._adapters[tag]
 
     def register_adapter(self, tag: str, adapter: PolymorphicAdapter[Any, Any]) -> None:
         """Register the given adapter instance with the given string tag."""
@@ -407,28 +311,17 @@ class Polymorphic:
 
 
     Like any other use of `typing.Annotated`, to static type checkers, the type
-    is simply `Base`.  For serialization, only objects whose types have been
-    registered with tags may be saved and loaded (unless tag extraction is
-    customized via the ``get_tag`` argument, and there is no runtime check that
-    loaded objects actually inherit from the annotated type (which allows it to
-    be a `Protocol` or `typing.Union` rather than a true base class).
+    of ``Holder.p``  is simply `Base`.  For serialization, only objects whose
+    types have been registered with tags may be saved and loaded (unless tag
+    extraction is customized via the ``get_tag`` argument, and there is no
+    runtime check that loaded objects actually inherit from the annotated type
+    (which allows it to be a `Protocol` or `typing.Union` rather than a true
+    base class).
 
-    In addition, adapters must be associated with each tag via a JSON config
-    field whose path is included in the ``SHOEFITS_ADAPTER_CONFIGS``
-    environment variable::
-
-        {
-            # Adapted types are configured with their adapter.
-            "derived1": "mymodule.Adapter1",
-            # Native types are configured directly.
-            "derived2": "mymodule.Derived2",
-        }
-
-    Values in the config file may be strings that are fully-qualified Python
-    symbols or nested dictionaries with a "type" string field and optional
-    "args" and "kwargs" fields, for adapters that take arguments at
-    construction.  Arguments must be simple JSON primitives (not lists or
-    dicts).
+    In addition, adapters must be associated with each tag via a
+    `PolymorphicAdapterRegistry`.  How a registry is populated in practice and
+    passed to serialization and serialization code (subclasses of
+    `WriteContext` and `ReadContext`) is left to downstream applications.
 
     When an adapter cannot be found for a serialized tag, the default behavior
     is to raise `PolymorphicReadError`.  Polymorphic fields can also be told to
